0ec331a39b63e39fd998189b3f5d01b29d9165c6
bruce.olson <bruce.olson@accenture.com>
2017-09-20 09:17:52 -0500

    semi working code
    having trouble with the .map function. believe that the typescript is not working right

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { HttpModule, Http, Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@Injectable()
 export class AppComponent {
-  title = 'app';
+  title = 'Cases';
+  private apiUrl = 'http://www.mocky.io/v2/590212490f00006b18d2cb05';
+  data: any = {};
+
+  constructor(private http: Http) {
+    console.log('hi');
+    this.getContacts();
+    this.getData();
+  }
+
+  getData() {
+    return this.http.get(this.apiUrl).subscribe(data => {;
+      //.map((res: Response) => res.json);
+  }
+
+  getContacts() {
+    this.getData().subscribe(data => {
+      console.log(data);
+      this.data = data;
+    });
+  }
 }
